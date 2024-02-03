44509db07319768f3ccd2afa974fa43edede1992
Oscar 4.0 <oscar2004feb@gmail.com>
2024-02-02 23:26:56 -0600

    Input Polling

--- ForByte/src/ForByte/Application.cpp
+++ ForByte/src/ForByte/Application.cpp
@@ -5,6 +5,8 @@
 
 #include <glad/glad.h>
 
+#include "Input.h"
+
 namespace ForByte {
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 
@@ -55,6 +57,9 @@ namespace ForByte {
 
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
+
+			auto [x, y] = Input::GetMousePosition();
+			FB_CORE_TRACE("{0}, {1}", x, y);
 
 			m_Window->OnUpdate();
 		}
