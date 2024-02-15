291dad4c612b3793fff38ce4552795d9ccb066f5
Oscar 4.0 <oscar2004feb@gmail.com>
2024-02-14 22:06:10 -0600

    Transform Matrix

--- Sandbox/src/SandBoxApp.cpp
+++ Sandbox/src/SandBoxApp.cpp
@@ -2,11 +2,13 @@
 
 #include "imgui/imgui.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 class ExampleLayer : public ForByte::Layer
 {
 public:
 	ExampleLayer() 
-		: Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f)
+		: Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f), m_SquarePosition(0.0f)
 	{
 		m_VertexArray.reset(ForByte::VertexArray::Create());
 
@@ -32,10 +34,10 @@ public:
 		m_VertexArray->SetIndexBuffer(indexBuffer);
 
 		float squareVertices[3 * 4] = {
-			-0.75f, -0.75f, 0.0f,
-			 0.75f, -0.75f, 0.0f,
-			 0.75f,  0.75f, 0.0f,
-			-0.75f,  0.75f, 0.0f
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			-0.5f,  0.5f, 0.0f
 		};
 
 		m_SquareVA.reset(ForByte::VertexArray::Create());
@@ -58,6 +60,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
             uniform mat4 u_ViewProjection;
+            uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -66,7 +69,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
         )";
 
@@ -93,13 +96,14 @@ public:
 			layout(location = 0) in vec3 a_Position;
 
             uniform mat4 u_ViewProjection;
+            uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
         )";
 
@@ -133,7 +137,6 @@ public:
 
 		if (ForByte::Input::IsKeyPressed(FB_KEY_A))
 			m_CameraRotation += m_CameraRotationSpeed * ts;
-
 		if (ForByte::Input::IsKeyPressed(FB_KEY_D))
 			m_CameraRotation -= m_CameraRotationSpeed * ts;
 
@@ -145,8 +148,19 @@ public:
 
 		ForByte::Renderer::BeginScene(m_Camera);
 
-		ForByte::Renderer::Submit(m_BlueShader, m_SquareVA);
-		ForByte::Renderer::Submit(m_Shader, m_VertexArray);
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for (int y = 0; y < 20; y++)
+		{
+			for (int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				ForByte::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+			}
+		}
+	
+		//ForByte::Renderer::Submit(m_Shader, m_VertexArray);
 
 		ForByte::Renderer::EndScene();
 
