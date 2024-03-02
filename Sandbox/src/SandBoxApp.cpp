6b74324b956f7a50cb53e8bd1f3844c2d5f56a5f
Oscar 4.0 <oscar2004feb@gmail.com>
2024-03-01 23:51:54 -0600

    Shader Assets Files

--- Sandbox/src/SandBoxApp.cpp
+++ Sandbox/src/SandBoxApp.cpp
@@ -129,42 +129,10 @@ public:
 
 		m_FlatColorShader.reset(ForByte::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 
-		std::string textureShaderVertexSrc = R"(
-			#version 330 core
-			
-			layout(location = 0) in vec3 a_Position;
-			layout(location = 1) in vec2 a_TexCoord;
-
-            uniform mat4 u_ViewProjection;
-            uniform mat4 u_Transform;
-
-			out vec2 v_TexCoord;
-
-			void main()
-			{
-				v_TexCoord = a_TexCoord;
-				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
-			}
-        )";
-
-		std::string textureShaderFragmentSrc = R"(
-			#version 330 core
-			
-			layout(location = 0) out vec4 color;
-
-			uniform sampler2D u_Texture;
-
-			in vec2 v_TexCoord;
-
-			void main()
-			{
-				color = texture(u_Texture, v_TexCoord);
-			}
-        )";
-
-		m_TextureShader.reset(ForByte::Shader::Create(textureShaderVertexSrc, textureShaderFragmentSrc));
+		m_TextureShader.reset(ForByte::Shader::Create("assets/shaders/Texture.glsl"));
 
 		m_Texture = ForByte::Texture2D::Create("assets/textures/Checkerboard.png");
+		m_ChernologoTexture = ForByte::Texture2D::Create("assets/textures/ChernoLogo.png");
 
 		std::dynamic_pointer_cast<ForByte::OpenGLShader>(m_TextureShader)->Bind();
 		std::dynamic_pointer_cast<ForByte::OpenGLShader>(m_TextureShader)->UploadUniformInt("u_Texture", 0);
@@ -212,6 +180,9 @@ public:
 
 		m_Texture->Bind();
 		ForByte::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
+
+		m_ChernologoTexture->Bind();
+		ForByte::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
 		//ForByte::Renderer::Submit(m_Shader, m_VertexArray);
 
 		ForByte::Renderer::EndScene();
@@ -241,7 +212,7 @@ private:
 	ForByte::Ref<ForByte::Shader> m_FlatColorShader, m_TextureShader;
 	ForByte::Ref<ForByte::VertexArray> m_SquareVA;
 
-	ForByte::Ref<ForByte::Texture2D> m_Texture;
+	ForByte::Ref<ForByte::Texture2D> m_Texture, m_ChernologoTexture;
 
 	ForByte::OrthographicCamera m_Camera;
 	glm::vec3 m_CameraPosition;
